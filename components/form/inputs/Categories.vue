1738cd4a37781af7b3e0f27f5e1b7927b6a90923
Jacob Carpenter <jacob@jacobpaulcarpenter.com>
2023-07-23 17:46:38 -0500

    Update Categories.vue to only have necessary logic for operation

--- components/form/inputs/Categories.vue
+++ components/form/inputs/Categories.vue
@@ -1,34 +1,12 @@
 <template>
-  <component :is="field.value.displayType" :field="field.value" />
+  <component :is="field.displayType" :field="field" />
 </template>
 
 <script setup>
-import Checkboxes from "@/components/form/inputs/Checkboxes.vue";
-import Dropdown from "@/components/form/inputs/Dropdown.vue";
-import Radio from "@/components/form/inputs/Radio.vue";
-
 const props = defineProps({
-  form: {
-    type: Object,
-    default: Object,
-  },
-
   field: {
     type: Object,
     default: Object,
   },
-});
-
-const suffix = computed(() => {
-  if (field.value.displayType !== "checkboxes") {
-    return [""];
-  }
-  return [];
-});
-
-onMounted(() => {
-  field.value.options = field.value.categories.map((option) => {
-    return { label: option.title, value: option.id };
-  });
 });
 </script>
