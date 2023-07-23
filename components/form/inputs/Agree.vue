71675ee53ad5424c400b496cd896d657d51db6e0
Jacob Carpenter <jacob@jacobpaulcarpenter.com>
2023-07-23 17:45:01 -0500

    Update Agree.vue to correct composables + auto imported field instructions/label components

--- components/form/inputs/Agree.vue
+++ components/form/inputs/Agree.vue
@@ -1,58 +1,53 @@
 <template>
   <div>
-    <FieldInstructions :field="field.value" position="above-input" />
+    <IncludesFieldInstructions :field="field" position="above-input" />
 
     <div class="mt-2 bg-white border border-gray-300">
       <input
         type="hidden"
         aria-checked="false"
         aria-hidden="true"
-        :name="useName()"
+        :name="getName()"
         value=""
       />
 
       <div class="flex items-center">
         <div class="p-6 border-r border-gray-300">
           <input
-            :id="manualId ? manualId : useId()"
-            :name="useName()"
+            :id="manualId ? manualId : getId()"
+            :name="getName()"
             value="1"
             type="checkbox"
-            :required="field.value.required"
+            :required="field.required"
             class="w-6 h-6 border border-gray-300 rounded-full cursor-pointer text-green-meta focus:ring-0"
             @click="$emit('inputClicked', $event.target.checked)"
           />
         </div>
         <div class="px-6">
           <label
-            :for="manualId ? manualId : useId()"
+            :for="manualId ? manualId : getId()"
             class="block text-sm font-light text-gray-500 cursor-pointer"
           >
             {{ fieldLabel }}<br />
             <!-- eslint-disable -->
-            <span v-html="field.value.descriptionHtml"></span>
+            <span v-html="field.descriptionHtml"></span>
             <!-- eslint-enable -->
           </label>
         </div>
       </div>
     </div>
 
-    <FieldInstructions :field="field.value" position="below-input" />
+    <IncludesFieldInstructions :field="field" position="below-input" />
 
-    <FieldLabel :id="useId()" :field="field.value" position="below-input" />
+    <IncludesFieldLabel :id="getId()" :field="field" position="below-input" />
   </div>
 </template>
 
 <script setup>
 const manualId = ref(null);
-const { useId, useName } = useFieldMixin();
+const { getId, getName } = useFieldMixin();
 
 const props = defineProps({
-  form: {
-    type: Object,
-    default: Object,
-  },
-
   field: {
     type: Object,
     default: Object,
