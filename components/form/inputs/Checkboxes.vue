cb1b00ee1e65cce2c112647d51dcae6b8dba6466
Jacob Carpenter <jacob@jacobpaulcarpenter.com>
2023-07-23 17:51:23 -0500

    Update Checkboxes.vue to use correct composables + adjusting `useName` on individual checkboxes

--- components/form/inputs/Checkboxes.vue
+++ components/form/inputs/Checkboxes.vue
@@ -1,5 +1,5 @@
 <template>
-  <fieldset :aria-labelledby="useId()">
+  <fieldset :aria-labelledby="getId()">
     <legend class="block mb-1 text-sm font-light text-gray-500">
       {{ field.label }}
       <span v-if="field.required" class="text-red-500">*</span>
@@ -14,15 +14,15 @@
         class="flex items-center"
       >
         <input
-          :id="useId([index])"
-          :name="useName([''])"
+          :id="getId([index])"
+          :name="getName([option.value])"
           :value="option.value"
           :required="field.required"
           type="checkbox"
           class="w-4 h-4 text-blue-100 border-gray-300 rounded focus:ring-blue-100"
         />
 
-        <label :for="useId([index])" class="block ml-3 text-sm text-gray-500">
+        <label :for="getId([index])" class="block ml-3 text-sm text-gray-500">
           {{ option.label }}
         </label>
       </div>
@@ -33,19 +33,12 @@
 </template>
 
 <script setup>
-import FieldInstructions from "@/components/form/inputs/includes/FieldInstructions.vue";
-
 const props = defineProps({
-  form: {
-    type: Object,
-    default: Object,
-  },
-
   field: {
     type: Object,
     default: Object,
   },
 });
 
-const { useId, useName } = useFieldMixin(props);
+const { getId, getName } = useFieldMixin(props);
 </script>
